bee85d2c597fa15bc1b8ed59d5ce66b5827182aa
jessl314 <jlchrysanthemum456@gmail.com>
2025-06-26 13:31:10 -0700

    added the rover container

--- marsroverimages/src/App.jsx
+++ marsroverimages/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
 import './App.css'
+import RoverContainer from './components/RoverContainer'
 
 function App() {
   return (
     <>
-      
+      <RoverContainer/>
     </>
   )
 }
---
+++ marsroverimages/src/components/RoverContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RoverContainer.css'
+
+const RoverContainer = () => {
+
+    return (
+        <div className="rover-container">
+        </div>
+    );
+}
+
+export default RoverContainer;
